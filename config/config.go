e3d64a5830552898f3fe069cf988489f85a7acba
AxisZql <mifei23333@outlook.com>
2022-06-03 11:04:22 +0800

    feat:initialize the basic configuration structure✨

--- config/config.go
+++ config/config.go
@@ -1,1 +1,146 @@
 package config
+
+import (
+	"github.com/spf13/viper"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+)
+
+/**
+*Author: AxisZql
+*Date: 2022-5-31
+*DESC: the configuration parse for this project
+ */
+
+type Config struct {
+	Common struct {
+		Db struct {
+			Address  string `mapstructure:"address"`
+			Username string `mapstructure:"username"`
+			Password string `mapstructure:"password"`
+			DbName   string `mapstructure:"dbName"`
+		} `mapstructure:"db"`
+
+		Redis struct {
+			Address  string `mapstructure:"address"`
+			Password string `mapstructure:"password"`
+			Db       int    `mapstructure:"db"`
+		} `mapstructure:"redis"`
+
+		Kafka struct {
+			Address  string `mapstructure:"address"`
+			Username string `mapstructure:"username"`
+			Password string `mapstructure:"password"`
+		}
+
+		Etcd struct {
+			Address           string `mapstructure:"address"`
+			BasePath          string `mapstructure:"bsePath"`
+			ServerPathLogic   string `mapstructure:"serverPathLogic"`
+			ServerPathConnect string `mapstructure:"serverPathConnect"`
+			Username          string `mapstructure:"username"`
+			Password          string `mapstructure:"password"`
+			ConnectionTimeout int    `mapstructure:"connectionTimeout"`
+		} `mapstructure:"etcd"`
+	}
+	Api struct {
+		Api struct {
+			Host string `mapstructure:"host"`
+			Port int    `mapstructure:"port"`
+		} `mapstructure:"api"`
+	}
+
+	LogicRpc struct {
+		Logic struct {
+			RpcAddress string `mapstructure:"rpcAddress"`
+			CerPath    string `mapstructure:"cerPath"`
+			KeyPath    string `mapstructure:"keyPath"`
+		} `mapstructure:"logic"`
+	}
+}
+
+var (
+	once sync.Once
+	conf Config
+)
+
+func GetConfig() *Config {
+	InitConfig()
+	return &conf
+}
+
+func InitConfig() {
+	// once.Do单例模式
+	once.Do(func() {
+		env := GetGinRunMode()
+		configFilePath := GetCurrentDirPath() + "/" + env + "/"
+		viper.SetConfigType("toml")
+		viper.SetConfigName("api")
+		viper.AddConfigPath(configFilePath)
+		err := viper.ReadInConfig()
+		if err != nil {
+			panic(err)
+		}
+		viper.SetConfigName("common")
+		err = viper.MergeInConfig()
+		if err != nil {
+			panic(err)
+		}
+		viper.SetConfigName("task")
+		err = viper.MergeInConfig()
+		if err != nil {
+			panic(err)
+		}
+		viper.SetConfigName("connect")
+		err = viper.MergeInConfig()
+		if err != nil {
+			panic(err)
+		}
+		viper.SetConfigName("logic")
+		err = viper.MergeInConfig()
+		if err != nil {
+			panic(err)
+		}
+		viper.SetConfigName("task")
+		err = viper.MergeInConfig()
+		if err != nil {
+			panic(err)
+		}
+		_ = viper.Unmarshal(&conf.Api)
+		_ = viper.Unmarshal(&conf.Common)
+		//_ = viper.Unmarshal(&conf.Connect)
+		_ = viper.Unmarshal(&conf.LogicRpc)
+		//_ = viper.Unmarshal(&conf.Task)
+
+	})
+}
+
+// GetCurrentDirPath 获取config的绝对路径
+func GetCurrentDirPath() string {
+	_, filename, _, _ := runtime.Caller(1)
+	aPath := strings.Split(filename, "/")
+	// 将../config/config.go 转换为../config/
+	dir := strings.Join(aPath[:len(aPath)-1], "/")
+	return dir
+}
+
+func GetRunMode() string {
+	env := os.Getenv("RUN_MODE")
+	if env == "" {
+		return "dev"
+	}
+	return env
+}
+func GetGinRunMode() string {
+	env := GetRunMode()
+	if env == "dev" {
+		return "debug"
+	} else if env == "test" {
+		return "debug"
+	} else if env == "prod" {
+		return "release"
+	}
+	return "release"
+}
--- go.mod
+++ go.mod
@@ -3,26 +3,39 @@ module axisChat
 go 1.18
 
 require (
-	github.com/pkg/errors v0.8.1
+	github.com/pkg/errors v0.9.1
 	github.com/segmentio/kafka-go v0.4.31
 	google.golang.org/grpc v1.46.2
 	google.golang.org/protobuf v1.28.0
 )
 
 require (
-
 	github.com/coreos/etcd v3.3.27+incompatible // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.1.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.15.5 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pierrec/lz4/v4 v4.1.14 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.12.0 // indirect
+	github.com/subosito/gotenv v1.4.0 // indirect
 	go.etcd.io/etcd v3.3.27+incompatible // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
@@ -31,6 +44,9 @@ require (
 	golang.org/x/net v0.0.0-20220531201128-c960675eff93 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/genproto v0.0.0-20220519153652-3a47de7e79bd // indirect
+	gopkg.in/ini.v1 v1.66.6 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/gorm v1.23.5 // indirect
 )
