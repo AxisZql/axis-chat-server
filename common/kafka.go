3532401569319fa3b51a90642ac0bdb3724e4734
AxisZql <mifei23333@outlook.com>
2022-06-04 10:45:50 +0800

    perf:optimizing Kafka connection state maintenance✨

--- common/kafka.go
+++ common/kafka.go
@@ -7,31 +7,106 @@ package common
  */
 
 import (
+	"axisChat/utils/zlog"
 	"context"
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/segmentio/kafka-go"
-	"log"
+	"sync"
 	"time"
 )
 
-func TopicProduce(topic string, msg []byte) error {
+type KafkaProducerConn struct {
+	mutex      sync.RWMutex
+	SocketMap  map[string]*kafka.Conn
+	CreateTime map[string]time.Time // 记录连接建立的时间
+}
+
+var (
+	producerConnMap KafkaProducerConn
+	_once           sync.Once
+)
+
+const (
+	UserQueuePrefix  = "user_chat_%d"
+	GroupQueuePrefix = "group_chat_%d"
+)
+
+func watchLongTimeNotUseConn() {
+	ticker := time.NewTicker(5 * time.Second)
+	defer func() {
+		ticker.Stop()
+	}()
+	select {
+	case <-ticker.C:
+		producerConnMap.mutex.RLock()
+		for key, val := range producerConnMap.CreateTime {
+			// 超过5min没有使用的连接将会被断开
+			if time.Since(val) > 5*time.Minute {
+				producerConnMap.mutex.Lock()
+				err := producerConnMap.SocketMap[key].Close()
+				if err != nil {
+					zlog.Error(err.Error())
+				}
+				delete(producerConnMap.SocketMap, key)
+				delete(producerConnMap.CreateTime, key)
+				producerConnMap.mutex.Unlock()
+			}
+		}
+		producerConnMap.mutex.RUnlock()
+	}
+}
+
+func getProducerConn(objectId int64, _type string) (*kafka.Conn, error) {
+	_once.Do(func() {
+		// 监听并删除长时间不使用的连接
+		go watchLongTimeNotUseConn()
+	})
+	var topic = ""
+	if _type == "user" {
+		topic = fmt.Sprintf(UserQueuePrefix, objectId)
+	} else if _type == "group" {
+		topic = fmt.Sprintf(GroupQueuePrefix, objectId)
+	} else {
+		return nil, errors.New(fmt.Sprintf("_type = %s is not alllow", _type))
+	}
+	producerConnMap.mutex.RLock()
+	if conn, ok := producerConnMap.SocketMap[topic]; ok {
+		producerConnMap.mutex.Lock()
+		producerConnMap.CreateTime[topic] = time.Now()
+		producerConnMap.mutex.Unlock()
+		producerConnMap.mutex.RUnlock()
+		return conn, nil
+	}
+	producerConnMap.mutex.RUnlock()
 	// 为保证整体消息的有序性，每个topic的partition数为1
-	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, 0)
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, 1)
 	if err != nil {
 		err = errors.Wrap(err, "get topic:%s from Kafka failure")
-		return err
+		return nil, err
 	}
 	defer func() {
 		err := conn.Close()
 		if err != nil {
-			// TODO: print the error to log
-			fmt.Println(err)
+			zlog.Error(err.Error())
 		}
 	}()
 	err = conn.SetWriteDeadline(time.Now().Add(5 * time.Second))
 	if err != nil {
 		err = errors.Wrap(err, "conn.SetWriteDeadlin get err")
+		return nil, err
+	}
+	producerConnMap.mutex.Lock()
+	defer producerConnMap.mutex.Unlock()
+	producerConnMap.SocketMap[topic] = conn
+	producerConnMap.CreateTime[topic] = time.Now()
+	return conn, nil
+}
+
+func TopicProduce(objectId int64, _type string, msg []byte) error {
+	conn, err := getProducerConn(objectId, _type)
+	if err != nil {
+		zlog.Error(err.Error())
 		return err
 	}
 	_, err = conn.WriteMessages(kafka.Message{
@@ -39,36 +114,35 @@ func TopicProduce(topic string, msg []byte) error {
 	})
 	if err != nil {
 		err = errors.Wrap(err, "conn.WriteMessages get err")
+		zlog.Error(err.Error())
 		return err
 	}
 	return nil
 }
 
-func TopicConsume(cGroupId string, topic string) error {
+func GetConsumeReader(objectId int64, topic string) (*kafka.Reader, error) {
 	reader := kafka.NewReader(kafka.ReaderConfig{
 		Brokers:  []string{"localhost:9092"}, //topic所在的broker
 		Topic:    topic,
-		GroupID:  cGroupId, // 消费者在topic中的groupId，通过groupId可以获取当前消费的偏移量
-		MinBytes: 10e3,     //10KB
-		MaxBytes: 10e6,     //10MB
+		GroupID:  fmt.Sprintf("%d", objectId), // 消费者在topic中的groupId，通过groupId可以获取当前消费的偏移量,本项目中groupID设置为每个对象的id
+		MinBytes: 10e3,                        //10KB
+		MaxBytes: 10e6,                        //10MB
 	})
-	defer func() {
-		err := reader.Close()
-		// TODO: print the error to log
-		fmt.Println(err)
-	}()
+	return reader, nil
+}
 
+func TopicConsume(reader *kafka.Reader) error {
 	for {
 		msg, err := reader.FetchMessage(context.Background())
 		if err != nil {
-			err = errors.Wrap(err, fmt.Sprintf("read the msg from topic:%s get error", topic))
+			zlog.Error(err.Error())
 			return err
 		}
 		fmt.Println(msg.Offset, string(msg.Key), string(msg.Value))
 		// TODO：消费确认，提交偏移量，只有提交量偏移量才能证明消息被正常消费
 		if err := reader.CommitMessages(context.Background(), msg); err != nil {
-			log.Fatal("failed to commit messages:", err)
+			zlog.Error(fmt.Sprintf("failed to commit messages:%v", err))
+			return err
 		}
 	}
-
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module axisChat
 go 1.18
 
 require (
+	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/pkg/errors v0.9.1
 	github.com/segmentio/kafka-go v0.4.31
+	go.uber.org/zap v1.21.0
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
 	google.golang.org/grpc v1.46.2
 	google.golang.org/protobuf v1.28.0
 )
@@ -17,21 +20,23 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.15.5 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pierrec/lz4/v4 v4.1.14 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.12.0 // indirect
@@ -39,8 +44,6 @@ require (
 	go.etcd.io/etcd v3.3.27+incompatible // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.21.0 // indirect
-	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 	golang.org/x/net v0.0.0-20220531201128-c960675eff93 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
@@ -48,5 +51,6 @@ require (
 	gopkg.in/ini.v1 v1.66.6 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.3.4 // indirect
 	gorm.io/gorm v1.23.5 // indirect
 )
